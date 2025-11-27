05608f12b71ccacc9e4eb6d38b29bfb63673ff0f
Pruthvi17-stu <pruthviindalkar@gmail.com>
2025-11-27 17:19:05 +0530

    Creating a password adapter

---
+++ app/src/main/java/com/example/passcrypter/addandmanagepage.java
@@ -0,0 +1,32 @@
+package com.example.passcrypter;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class addandmanagepage extends AppCompatActivity {
+    private RecyclerView passwordrecyclerView;
+    private PasswordAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.addandmanagepage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.addandmanage), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            return insets;
+
+        });
+
+
+
+    }
+}
